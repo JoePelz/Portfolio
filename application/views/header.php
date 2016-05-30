623653e57c44eda0be20b4d57b4df88ac51c0d53
Joe Pelz <joe.pelz@gmail.com>
2016-05-30 12:00:50 -0700

    removed commented HTML

--- application/views/header.php
+++ application/views/header.php
@@ -1,13 +1,6 @@
 <header>
   <div id="LogoBlock"></div>
     <h2>Projects, portfolio, and personal blog of Joe Pelz</h2>
-    <!--<ul>
-      <li><a href="./#programming"><div>Programming</div></a>
-      </li><li><a href="./#school"><div>School Projects</div></a>
-      </li><li><a href="./#vfx"><div>Visual Effects</div></a>
-      </li><li><a href="./#inspiration"><div>My Inspiration</div></a>
-      </li>
-    </ul>-->
     <ul>
       {sections}<li><a href="/#{sectionTitle}"><div>{sectionTitle}</div></a></li>{/sections}
     </ul>
