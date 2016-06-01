ed34325ffdbc8d59a8de4517d3dfa773895527c1
Joe Pelz <joe.pelz@gmail.com>
2016-06-01 12:15:56 -0700

    made logo a link back to home page

--- application/views/header.php
+++ application/views/header.php
@@ -1,8 +1,8 @@
 <header>
-  <div id="LogoBlock"></div>
-    <h2>Projects, portfolio, and personal blog of Joe Pelz</h2>
-    <ul>
-      {sections}<li><a href="/#{sectionTitle}"><div>{sectionTitle}</div></a></li>{/sections}
-    </ul>
+  <div id="LogoBlock"><a href="/"></a></div>
+  <h2>Projects, portfolio, and personal work of Joe Pelz</h2>
+  <ul>
+    {sections}<li><a href="/#{sectionTitle}"><div>{sectionTitle}</div></a></li>{/sections}
+  </ul>
   <div class="rel"><div class="fadeout"></div></div>
 </header>
